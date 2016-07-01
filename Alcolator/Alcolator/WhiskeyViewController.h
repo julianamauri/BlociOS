b70cea5913d53dd5ea22a824ead8cd3b3d38c7e5
julianamauri <julianaporto.designer@gmail.com>
2016-07-01 21:32:02 +0100

    Checkpoint 20 iOS Alcolator completed

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Juliana Mauri on 01/07/2016.
+//  Copyright (c) 2016 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
